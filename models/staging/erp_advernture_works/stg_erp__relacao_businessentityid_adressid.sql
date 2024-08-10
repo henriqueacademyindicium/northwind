2a089f7546e517ef3f635c8b372f663a11fa584e
Henrique Rosa <henriqueacademyindicium@gmail.com>
2024-08-10 13:14:49 +0000

    ajustada questao sobre endereço. itens estavam duplicados por conta do tipo de endereço

--- models/staging/erp_advernture_works/stg_erp__relacao_businessentityid_adressid.sql
+++ models/staging/erp_advernture_works/stg_erp__relacao_businessentityid_adressid.sql
@@ -3,11 +3,27 @@ with
         select  
         cast(businessentityid as integer) as id_businessentity
         ,cast (addressid as integer) as id_endereco
-        --addresstypeid
-        --rowguid
-        --modifieddate
+        , addresstypeid 
+        ,rowguid
+        ,modifieddate
         from {{ source('erp adventure works', 'person_businessentityaddress') }}
     )
 
- select * 
- from fonte_businessentity_adress    
+    select  *
+    from fonte_businessentity_adress
+    where addresstypeid = 2
+    -- estamos considerando apenas a casa dos clientes (adresstype = 2)
+    
+
+ --5976
+ --996
+ --1072
+ --0550
+ --124
+ --7298
+ --6746
+
+ --select  id_businessentity
+ --from x   
+ --group by id_businessentity
+ --having count(*) > 1
